00c9a00d44089d52efb05837b6a1ab6ad45e7559
LAPTOP-7V78BBO2\ydf19 <ydf@xx.com>
2026-06-14 21:12:29 +0800

    1

---
+++ bfzs/init_loggers.py
@@ -0,0 +1,10 @@
+
+import nb_log
+
+
+nb_log.get_logger("lc_agent.debug",log_filename="lc_agent_debug.log",
+               error_log_filename="lc_agent_debug_error.log",)
+
+
+nb_log.get_logger("lc_agent.mcp",log_filename="lc_agent_mcp.log",
+               error_log_filename="lc_agent_mcp_error.log",)
